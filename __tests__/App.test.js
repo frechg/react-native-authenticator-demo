aed3f76a6273ae5b34ff2b0f798194968a3b4d0a
frechg <galenfrechette@gmail.com>
2022-06-22 17:00:13 +0700

    Adds jest setup fil with mock for react navigation

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react-native';
 
 import App from '../App';
 
 jest.useFakeTimers(); // without this, _reactNative.BackHandler.addEventListener causes errors
 
 describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
-  });
-
   it('renders correctly', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
+    const {debug} =render(<App />);
+    debug();
   });
 });
