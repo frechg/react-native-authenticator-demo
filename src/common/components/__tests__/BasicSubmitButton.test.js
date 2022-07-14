8384602033ceeeabd39b9f14f69a9275086d8ba5
frechg <galenfrechette@gmail.com>
2022-07-14 17:02:41 +0800

    Adds test coverage for BasicSubmitButton

---
+++ src/common/components/__tests__/BasicSubmitButton.test.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react-native';
+import { Formik, useFormikContext } from 'formik';
+import { BasicSubmitButton } from '../BasicSubmitButton';
+
+jest.mock('formik');
+
+describe('<BasicSubmitButton />', () => {
+  test('Renders a basic submit button', () => {
+    const handleSubmit = jest.fn();
+    const isSubmitting = false;
+    useFormikContext.mockReturnValue({handleSubmit, isSubmitting});
+
+    const title = 'Test Submit';
+    render(
+      <BasicSubmitButton title={title} />
+    )
+
+    expect(screen.getByText(title)).toBeTruthy();
+    expect(screen).toMatchSnapshot();
+  });
+
+  test('Calls the submit handler passed by the Formik context', async () => {
+    const handleSubmit = jest.fn();
+    const isSubmitting = false;
+    useFormikContext.mockReturnValue({handleSubmit, isSubmitting});
+
+    render(
+      <BasicSubmitButton title='Submit Test' />
+    )
+    
+    fireEvent.press(screen.getByText('Submit Test'));
+
+    await waitFor(() => {
+      expect(handleSubmit).toHaveBeenCalled();
+    });
+  });
+
+  test('Hides button title and shows loading indicator when submitting', async () => {
+    const handleSubmit = jest.fn();
+    const isSubmitting = true;
+    useFormikContext.mockReturnValue({handleSubmit, isSubmitting});
+
+    render(
+      <BasicSubmitButton title='Submit Test' />
+    )
+    
+    await waitFor(() => {
+      expect(screen.getByText('Submitting...')).toBeTruthy();
+    });
+  });
+});
